7175d7855e4d51ebc49c66654aaf0ea7742c63a3
aminn <aminn@pami.com>
2026-07-20 17:06:16 +0200

    add menubar und change pages

--- src/main/java/com/parmin/country/Contoller.java
+++ src/main/java/com/parmin/country/Contoller.java
@@ -1,6 +1,7 @@
 package com.parmin.country;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import tools.jackson.core.type.TypeReference;
 import tools.jackson.databind.ObjectMapper;
@@ -25,14 +27,33 @@ public class Contoller {
 	
 	@Autowired
 	private CountryService countryService;
+	List<MenuItem> menus = List.of(new MenuItem("Home","/home"),
+			new MenuItem("Search","search"),
+			new MenuItem("About","/about"));
 	
+	@GetMapping({"/" ,"/home"})
+	public String home(Model model) {
+		model.addAttribute("page","home");
+		model.addAttribute("menus" , menus);
 		
-	@GetMapping("/{country}")
-	String country(@PathVariable String country,Model model) {
-		Map<String, Object> countryInfo = new HashMap<String, Object>();
+	    return "country/country";
+	}
+	
+	@GetMapping("/about")
+	public String about(Model model) {
+		model.addAttribute("page","about");
+		model.addAttribute("menus" , menus);
+		
+	    return "country/country";
+	}
+
+	@GetMapping("/search")
+	String country(@RequestParam (required = false) String searchtext , Model model) {
+		model.addAttribute("page","search");
+		model.addAttribute("menus" , menus);		
 		String response ="";
 		try {
-			response = countryService.getCountry(country);
+			response = countryService.getCountry(searchtext);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -40,20 +61,37 @@ public class Contoller {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-			
+			if(response ==null || response.isBlank()) {
+				model.addAttribute("showResult", false);
+				return "country/country";
+			}
+			model.addAttribute("showResult", true);
 		
 		ObjectMapper objectMapper = new ObjectMapper();
+		
+		ArrayList<Map<String, Object>> countryInfoList = new ArrayList<Map<String, Object>>();
+		
 		Map<String, Object> countryInfoAllData =
 			    objectMapper.readValue(response, new TypeReference<Map<String, Object>>() {});
 		Map<String, Object> countryInfoData =
 				(Map<String, Object>) countryInfoAllData.get("data");
 		List<Map<String, Object>> countryInfoObjects =
 				 (List<Map<String, Object>>) countryInfoData.get("objects");
+
 		
-	    if(!countryInfoObjects.isEmpty()) countryInfo = (Map<String, Object>) countryInfoObjects.get(0);
-			   
+
 		
-		model.addAttribute("countryinfo" , countryInfo);
+		if(countryInfoObjects!= null && !countryInfoObjects.isEmpty()) {
+			
+			for (Map<String, Object>  countryInfo: countryInfoObjects) {		
+				
+				 countryInfoList.add(countryInfo);
+			}
+				
+		}
+		
+		model.addAttribute("countryInfoList" , countryInfoList);
+		
 		return "country/country";
 	}
 	
---
+++ src/main/java/com/parmin/country/MenuItem.java
@@ -0,0 +1,12 @@
+package com.parmin.country;
+
+public class MenuItem {	
+	public String text;
+	public String URL;
+	public MenuItem(String text, String url) {
+		this.text = text;
+		URL = url;
+	}
+	
+	
+}
